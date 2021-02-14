139ee48350023895e97c388d3fe1a6a4183d0b6a
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-14 19:22:04 +0530

    edit go

--- main.go
+++ main.go
@@ -2,18 +2,103 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println("$ ")
+		fmt.Print("$ ")
+		hello()
 		cmdString, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprint(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, err)
 		}
-		fmt.Println(cmdString)
+		err = runCommand(cmdString)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
 	}
+}
+func runCommand(commandStr string) error {
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	switch arrCommandStr[0] {
+	case "exit":
+		os.Exit(0)
+	case "plus":
+		if len(arrCommandStr) < 3 {
+			return errors.New("Required for 2 arguments")
+		}
+		arrNum := []int64{}
+		for i, arg := range arrCommandStr {
+			if i == 0 {
+				continue
+			}
+			n, _ := strconv.ParseInt(arg, 10, 64)
+			arrNum = append(arrNum, n)
+		}
+		fmt.Fprintln(os.Stdout, arrNum[0]+arrNum[1])
+		return nil
+	case "substract":
+		if len(arrCommandStr) < 3 {
+			return errors.New("Required two arguments")
+		}
+		arrNum := []int64{}
+		for i, arg := range arrCommandStr {
+			if i == 0 {
+				continue
+			}
+			n, _ := strconv.ParseInt(arg, 10, 64)
+			arrNum = append(arrNum, n)
+		}
+		fmt.Fprintln(os.Stdout, arrNum[0]-arrNum[1])
+		return nil
+
+	case "multiply":
+		if len(arrCommandStr) < 3 {
+			return errors.New("Required two arguments")
+		}
+		arrNum := []int64{}
+		for i, arg := range arrCommandStr {
+			if i == 0 {
+				continue
+			}
+			n, _ := strconv.ParseInt(arg, 10, 64)
+			arrNum = append(arrNum, n)
+		}
+		fmt.Fprintln(os.Stdout, arrNum[0]*arrNum[1])
+		return nil
+
+	case "divide":
+		if len(arrCommandStr) < 3 {
+			return errors.New("Required two arguments")
+		}
+		arrNum := []int64{}
+		for i, arg := range arrCommandStr {
+			if i == 0 {
+				continue
+			}
+			n, _ := strconv.ParseInt(arg, 10, 64)
+			arrNum = append(arrNum, n)
+		}
+		fmt.Fprintln(os.Stdout, arrNum[0]/arrNum[1])
+		return nil
+	}
+	cmd := exec.Command(arrCommandStr[0], arrCommandStr[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
+
+func hello() {
+	fmt.Println("-Shell")
+	fmt.Println("Use at your own risk")
+	fmt.Println("Author: Dinuda Yaggahavita")
 }
